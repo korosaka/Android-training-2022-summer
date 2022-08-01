6093cec898510781d88e0ec5a9111c4024acb2ae
koro <koro.nn99@gmail.com>
2022-08-01 04:14:25 -0700

    get data by async using Coroutines

--- app/src/main/java/com/example/androidtraining20220719/view_model/TopPageViewModel.kt
+++ app/src/main/java/com/example/androidtraining20220719/view_model/TopPageViewModel.kt
@@ -1,25 +1,44 @@
 package com.example.androidtraining20220719.view_model
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.androidtraining20220719.model.CharacterHeaderData
 import com.example.androidtraining20220719.model.MockData
+import kotlinx.coroutines.*
 
 
 // To use context in ViewModel, AndroidViewModel is used
 class TopPageViewModel(application: Application) : AndroidViewModel(application) {
 
-    var characters = MutableLiveData<List<CharacterHeaderData>>()
+    val characters = MutableLiveData<List<CharacterHeaderData>>()
 
+    /**
+     * To keep same instance for List, localCharactersData is used
+     * because LiveData.value can't use clear(), for example.
+     * The reason to keep the List instance is to avoid recreate Adapter for ListView and RecyclerView.
+     * If the different instance is set to the value, notify method never work.
+     */
+    private val localCharactersData: MutableList<CharacterHeaderData> = mutableListOf()
 
     init {
-        characters.value = listOf()
-        val context = getApplication<Application>().applicationContext
-        //TODO do in async
-        characters.value = MockData(context).getCharactersData()
+        characters.value = localCharactersData
+        fetchData()
     }
 
+    private fun fetchData() {
+        val context = getApplication<Application>().applicationContext
+        viewModelScope.launch(Dispatchers.Main) {
+            val data = withContext(Dispatchers.IO) {
+                MockData(context).getCharactersData()
+            }
+            localCharactersData.clear()
+            for (character in data) {
+                delay(200)
+                localCharactersData.add(character)
+                // to be observed, the data need to be set after local data is updated
+                characters.value = localCharactersData
+            }
+        }
+    }
 }
