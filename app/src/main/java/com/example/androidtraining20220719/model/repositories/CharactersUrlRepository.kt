7b86a3862f3fcc6e4157fa6a897588671ebb1092
koro <koro.nn99@gmail.com>
2022-08-02 17:50:37 -0700

    create base repository class

--- app/src/main/java/com/example/androidtraining20220719/model/repositories/CharactersUrlRepository.kt
+++ app/src/main/java/com/example/androidtraining20220719/model/repositories/CharactersUrlRepository.kt
@@ -1,22 +1,12 @@
 package com.example.androidtraining20220719.model.repositories
 
-import com.example.androidtraining20220719.model.services.CharacterService
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-class CharactersUrlRepository {
+class CharactersUrlRepository: BaseRepository() {
 
     fun fetchCharactersApiUrl(): String? {
-        val service = createRetrofit().create(CharacterService::class.java)
-        val response = service.fetchCharactersURL().execute()
+        val response =
+            createCharacterService()
+                .fetchCharactersURL()
+                .execute()
         return response.body()?.characters
-    }
-
-    private fun createRetrofit(): Retrofit {
-        return Retrofit
-            .Builder()
-            .baseUrl("https://rickandmortyapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
     }
 }
