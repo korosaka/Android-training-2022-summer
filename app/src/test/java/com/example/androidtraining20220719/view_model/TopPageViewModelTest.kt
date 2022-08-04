acd38826e2746475aab874841b9eb4beb8708b38
koro <koro.nn99@gmail.com>
2022-08-03 22:47:02 -0700

    develop LiveData's value test in VM

--- app/src/test/java/com/example/androidtraining20220719/view_model/TopPageViewModelTest.kt
+++ app/src/test/java/com/example/androidtraining20220719/view_model/TopPageViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.example.androidtraining20220719.view_model
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.androidtraining20220719.model.CharacterHeaderData
+import com.example.androidtraining20220719.repositories.CharactersRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.setMain
@@ -27,19 +29,59 @@ class TopPageViewModelTest {
         //Avoid Main
         Dispatchers.setMain(Dispatchers.Unconfined)
 
-        viewModel = TopPageViewModel()
+        viewModel = TopPageViewModel(createMockCharactersRepo())
     }
 
     @Test
     fun statusMessageTest() {
         assertEquals("default", viewModel.statusMessage.value)
         runBlocking {
-            viewModel.fetchCharactersExcludeImage()
+            viewModel.fetchCharactersExceptImage()
         }
 
 //        latch.await()
 //        latch.countDown()
         assertEquals("finish fetching data except images", viewModel.statusMessage.value)
+    }
+
+    @Test
+    fun liveCharactersDataTest() {
+        assertTrue(viewModel.characters.value!!.isEmpty())
+        runBlocking {
+            viewModel.fetchCharactersExceptImage()
+        }
+        assertEquals(EXPECTED_CHARACTERS_SIZE, viewModel.characters.value!!.size)
+        for (character in viewModel.characters.value!!) {
+            assertTrue(character.name.isNotEmpty())
+            assertEquals(null, character.image)
+        }
+    }
+
+    companion object {
+        const val EXPECTED_CHARACTERS_SIZE = 30
+    }
+
+
+    private fun createMockCharactersRepo(): CharactersRepository {
+        return object : CharactersRepository {
+            override fun fetchCharactersExceptImage(): List<CharacterHeaderData> {
+                val mutableList = mutableListOf<CharacterHeaderData>()
+                for (i in 0 until EXPECTED_CHARACTERS_SIZE) {
+                    mutableList.add(createRandomCharacterHeader())
+                }
+                return mutableList.toList()
+            }
+
+            private fun createRandomCharacterHeader(): CharacterHeaderData {
+                val id = (1..20).random().toString()
+                return CharacterHeaderData(
+                    id,
+                    "(ID: $id) Test Character",
+                    "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                    null
+                )
+            }
+        }
     }
 
 }
